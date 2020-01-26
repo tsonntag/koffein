f170d14c415b2ab737a919807780f81217e06898
Thomas Sonntag <git@sonntagsbox.de>
2020-01-26 16:40:28 +0100

    random color

--- random_color.rb
+++ random_color.rb
@@ -1,34 +1,92 @@
 # coding: utf-8
 
-class Color
-  class << self
+class Arr
+  def initialize els, indent = ""
+    @els = els
+    @indent = indent
+  end
 
-    @@colors = %w( red green black magenta darkorange gold )
-    @@farben = %w( rot grün gelb blau schwarz weiß lila )
-
-    @@ncolors = @@colors.size
-    @@nfarben = @@farben.size
-
-    def list n
-      lines = (1..n).map do
-        rcolor = @@colors[Random.rand(@@ncolors)]
-        rfarbe = @@farben[Random.rand(@@nfarben)]
-        "  ( \"#{rfarbe}\", \"#{rcolor}\" )"
-      end
-      [ "[" ] + lines + [ "]"]
+  def elmed 
+    if @els.is_a? String
+#     @indent + "" + @els
+      @els
+    else
+      ar = @els.map do |el|
+        Arr.new(el,"  ").elmed()
+      end.join( "\n" + @indent + ", " )
+      "[" + 
+        "\n" +
+        @indent + "  " + ar + "\n" +
+        @indent + "]"
     end
+  end
 
-    def lists n, nlist
-      lists = (1..nlist).map do
-        list n
+=begin
+  def elmed
+    lines = @els.each_with_index.map do |el,i| 
+       if el.is_a? String
+      #   @indent + (i == 0 ? "  " : ", ") + el 
+         el
+       else
+          Arr.new(el, "  ").elmed
       end
-
-      [ "[" ] + lists + [ "]"]
     end
+    lines = lines.each_with_index.map do |el,i| 
+      if el.is_a? String
+         @indent + (i == 0 ? "  " : ", ") + el 
+      else
+        el
+      end
+    end
+    [@indent + "["] +  lines + [@indent + "]"]
+  end
+=end
+
+end
+ 
+class ColorList
+  def initialize(nitem)
+    @nitem = nitem
+  end
+
+  @@colors = %w( red green black magenta darkorange gold )
+  @@farben = %w( rot grün gelb blau schwarz weiß lila )
+
+  @@ncolors = @@colors.size
+  @@nfarben = @@farben.size
+
+  def list 
+    (1..@nitem).map do |i|
+      rcolor = @@colors[Random.rand(@@ncolors)]
+      rfarbe = @@farben[Random.rand(@@nfarben)]
+        "( \"#{rfarbe}\", \"#{rcolor}\" )"
+    end
+  end
+
+  def self.list nitem
+    ColorList.new(nitem).list
   end
 end
 
-n = ARGV[0].to_i
+nitem = ARGV[0].to_i
 nlist = ARGV[1].to_i
 
-puts Color.lists(n, nlist)
+#puts Arr.elmed(ColorList.list(nitem))
+
+#puts "==="
+#lists = (1..nlist).map do |l|
+#  x = ColorList.list(nitem)
+#  y = Arr.elmed(x, "")
+#  puts "Y"
+#  puts y.class
+#  puts y
+#  y
+#end
+#
+#puts "XXXX"
+#puts Arr.new(lists).elmed("  ")
+#
+#puts Arr.new(%w(A B)).elmed
+puts Arr.new([%w(A B), %w(C D)]).elmed
+
+
